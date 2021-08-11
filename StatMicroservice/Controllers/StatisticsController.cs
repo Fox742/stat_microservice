47d86aa882b1329e1a56d8594615b22e34879001
Your Name <igr.khitrov@gmail.com>
2021-08-11 17:26:40 +0300

    Added StatisticController

---
+++ StatMicroservice/Controllers/StatisticsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StatMicroservice.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StatisticsController : ControllerBase
+    {
+        private readonly ILogger<StatisticsController> _logger;
+        private readonly IConfiguration _configuration;
+        public StatisticsController(ILogger<StatisticsController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public async Task<IActionResult> Save(string key, string eventJson, DateTime? clientDT = null)
+        {
+
+
+
+            return Ok();
+        }
+
+    }
+}
