f9257bcdfde1c756ce08e04639b7bd45abee9e12
Your Name <igr.khitrov@gmail.com>
2021-08-12 23:01:13 +0300

    Added comments

--- StatMicroservice/Sorter.cs
+++ StatMicroservice/Sorter.cs
@@ -12,18 +12,23 @@ namespace StatMicroservice
         {
             List<KeyValuePair<string, int>> result = new List<KeyValuePair<string, int>>();
             int index = 0;
+            
+            // Идём по неотсортированному списку
             foreach (var record in toSort)
             {
                 string key = null;
 
+                // У каждой записи берём поле с JSON-ом и парсим его
                 var currentJSON = JObject.Parse(record["eventJson"]);
 
+                // Берём значение этого ключевого поля и сохраняем в переменную key
                 JToken jToken;
                 if (currentJSON.TryGetValue(field, out jToken) && jToken.Type == JTokenType.String)
                 {
                     key = jToken.Value<string>() ?? null;
                 }
 
+                // Записываем в список пару (Значение поля JSON-а, номер записи по порядку в неотсортированном списке)
                 result.Add(new KeyValuePair<string, int>(key, index));
 
                 index++;
@@ -33,15 +38,17 @@ namespace StatMicroservice
 
         public void Sort(ref List<Dictionary<string, string>>toSort, string field)
         {
+            // Создаём список из таких пар: Pair<Значение поля в JSON-е, Индекс строки>
             List<KeyValuePair<string, int>> ValueIndexList = getValueIndexList(toSort,field);
 
-            // Сортируем ValueIndexList
+            // Сортируем пары по значению поля JSON-а
             ValueIndexList.Sort((pair1, pair2) => string.Compare(pair1.Key, pair2.Key));
 
             // Создаём новый список
             var result = new List<Dictionary<string, string>>();
             
-            // Наполняем новый список
+            // Наполняем новый список теми элементами, индекс которых указывают индексы в отсортированном
+            //   списке пар
             foreach(var onePair in ValueIndexList)
             {
                 result.Add( toSort[onePair.Value] );
